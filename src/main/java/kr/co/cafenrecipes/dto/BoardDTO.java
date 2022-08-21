8b082487f37c6ecc3b2144b1ef2ea92c778f99e2
yunabaek <95681149+yunabaek@users.noreply.github.com>
2022-08-21 18:47:30 +0900

    깃허브 연결 여부 확인 차 BoardDTO만 업로드

---
+++ src/main/java/kr/co/cafenrecipes/dto/BoardDTO.java
@@ -0,0 +1,62 @@
+package kr.co.cafenrecipes.dto;
+
+import java.sql.Date;
+
+public class BoardDTO {
+
+	private int idx;
+	private String name;
+	private String subject;
+	private String content;
+	private int bHit;
+	private Date reg_date;
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getbHit() {
+		return bHit;
+	}
+
+	public void setbHit(int bHit) {
+		this.bHit = bHit;
+	}
+
+	public Date getReg_date() {
+		return reg_date;
+	}
+
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+
+}
